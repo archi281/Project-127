d7227c30c572204254576ac25cc566bcadd1c7f2
MidnightLake_281 <104713230+archi281@users.noreply.github.com>
2024-01-06 17:21:04 +0000

    Add files via upload

---
+++ bright_stars_data_scraping.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+browser = webdriver.Chrome("D:/Setup/chromedriver_win32/chromedriver.exe")
+browser.get(START_URL)
+time.sleep(10)
+stars_data = []
+
+def scrape():
+   
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+
+        bright_star_table = soup.find("table", attrs={"class", "wikitable"})
+
+        table_body = bright_star_table.find('tbody')
+
+        table_rows = table_body.find_all('tr')
+
+        for row in table_rows:
+            table_cols = row.find_all('td')
+            temp_list = []
+            for col_data in table_cols:
+                data = col_data.text.strip()
+                temp_list.append(data)
+            stars_data.append(temp_list)
+  
+scrape()
+
+for i in range(0,len(stars_data)):
+    
+    Star_names =stars_data [i][1]
+    Distance = stars_data[i][3]
+    Mass = stars_data[i][5]
+    Radius = stars_data[i][6]
+
+    required_data = [Star_names, Distance, Mass, Radius,]
+    stars_data.append(required_data)
+
+print(stars_data)
+
+headers = ['Star_name','Distance','Mass','Radius','Luminosity']   
+star_df_1 = pd.DataFrame(stars_data, columns=headers)
+star_df_1.to_csv('scraped_data.csv',index=True, index_label="id")
